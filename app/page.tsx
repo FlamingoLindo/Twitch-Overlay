8fcf241d68aa027f14970d6154d080cea1d3fdca
FlamingoLindo <vitorantunes2003@gmail.com>
2026-04-13 11:16:19 -0300

    feat: green-screen

--- app/page.tsx
+++ app/page.tsx
@@ -114,9 +114,9 @@ export default function Home() {
   if (loading) return <div>Loading...</div>
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-green-400">
       <main
-        className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"
+        className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-green-400 sm:items-start"
         onMouseDown={() => setSelectedId(null)}
       >
         <UploadBtn onCreateText={handleCreateText} onCreateFile={handleCreateFile} />
