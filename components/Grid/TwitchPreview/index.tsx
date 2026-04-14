7a9717a5a32b22075f63e1641e5d9ff5eb59f537
FlamingoLindo <vitorantunes2003@gmail.com>
2026-04-14 14:38:02 -0300

    fix: update TwitchPreview iframe source to correct channel and parent URL

--- components/Grid/TwitchPreview/index.tsx
+++ components/Grid/TwitchPreview/index.tsx
@@ -2,7 +2,7 @@ export default function TwitchPreview() {
     return (
         <div className="w-full h-full">
             <iframe
-                src="https://player.twitch.tv/?channel=forsen&parent=localhost&muted=true"
+                src="https://player.twitch.tv/?channel=flamingo_lindo&parent=overlay.flamingo-lindo.com.br&muted=true"
                 width="100%"
                 height="100%"
                 className="w-full h-full"
